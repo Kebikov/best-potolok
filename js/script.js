2948329c1df8a5e1fc025e57fa8183b050d64071
Gen <kebikovgen@gmail.com>
2022-07-05 00:42:09 +0300

    add start monday-block

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const 
-burgerSquare = document.querySelector('.burger__square'),
-menuList = document.querySelector('.menu__list'),
-menuLine = menuList.querySelectorAll('.menu__line');
+    burgerSquare = document.querySelector('.burger__square'),
+    menuList = document.querySelector('.menu__list'),
+    menuLine = menuList.querySelectorAll('.menu__line'),
+    burgerSpan = document.querySelector('.burger__span');
 
-burgerSquare.addEventListener('click', (e) => {
-    let target = e.target;
+burgerSquare.addEventListener('click', () => {
     let time = 0.3;
     menuLine.forEach(item => {
         item.style.transition = `all ${time}s ease`;
         item.classList.toggle('active');
         time += 0.1;
     });
+    burgerSpan.classList.toggle('active-burger');
 });
 
