0f1b433d254c50d305d7dace7b9be287586c7d2a
Gen <kebikovgen@gmail.com>
2022-07-14 18:19:25 +0300

    add in calculator, two select

--- js/script.js
+++ js/script.js
@@ -136,21 +136,45 @@ fetch('../ajax/foto_potolkov.json')
 
 //= калькулятор index   
 (function calcIndex () {
-    const selectSoffitTypeSelect = document.querySelector('.select-soffit-type__select');
-    const selectSoffitTypeSelectText = document.querySelector('.select-soffit-type__select-text');
-    const selectSoffitTypeLine = document.querySelectorAll('.select-soffit-type__line');
-    selectSoffitTypeSelect.addEventListener('click', (e) => {
-        selectSoffitTypeLine.forEach(item => {
-            item.classList.remove('line-none');
-            item.addEventListener('click', (e) => {
-                let targetLine = e.target;
-                selectSoffitTypeSelectText.textContent = targetLine.textContent;
-                selectSoffitTypeLine.forEach(item => {
-                    item.classList.add('line-none');
-                });
-            });
-        });
+    const selectSoffitTypeSelectAll = document.querySelectorAll('.select-soffit-type__select');
+
+    selectSoffitTypeSelectAll.forEach(select => {
+        select.addEventListener('click', selectClick);
     });
+
+    let lineAll;
+    let selectText;
+    function selectClick (e) {
+        let selectTarget = e.target.closest('.select-soffit-type__select').nextElementSibling;
+        let typeSelect = e.target.closest('.select-soffit-type__select');
+        selectText = typeSelect.querySelector('.select-soffit-type__select-text');
+        console.log(selectText);
+        lineAll = selectTarget.querySelectorAll('.select-soffit-type__line');
+        lineAll.forEach(line => {
+            line.addEventListener('click', lineClick);
+            line.classList.remove('line-none');
+        });
+    }
+    function lineClick (e) {
+        selectText.textContent = e.target.textContent;
+        lineAll.forEach(line => {
+            line.classList.add('line-none');
+            line.removeEventListener('click', lineClick);
+        });
+    }
+
+    
+        // selectSoffitTypeLine.forEach(item => {
+        //     item.classList.remove('line-none');
+        //     item.addEventListener('click', (e) => {
+        //         let targetLine = e.target;
+        //         selectSoffitTypeSelectText.textContent = targetLine.textContent;
+        //         selectSoffitTypeLine.forEach(item => {
+        //             item.classList.add('line-none');
+        //         });
+        //     });
+        // });
+    
 }());
 
 //конец страницы
