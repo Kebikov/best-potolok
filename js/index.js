da071fbbdd983ef8c4d71192ea4f3eb2878278fa
Gen <kebikovgen@gmail.com>
2022-08-02 22:36:49 +0300

    change calculator in  heders

--- js/index.js
+++ js/index.js
@@ -4,11 +4,26 @@ window.addEventListener('DOMContentLoaded', () => {
 (function calcM2 () {
     const praceMondeyInput = document.querySelector('.prace-mondey__input');
     const  praceMondeyByn = document.querySelector('.prace-mondey__byn');
+    const cursUsd = 2.6;
+    const works = 15;
+    let praceM2;
+    let pracePerimetr = 1;
+    let square;
+
+    inputSum();
     praceMondeyInput.addEventListener('input', () => {
-        let num = praceMondeyInput.value;
-        if(num.length > 3) praceMondeyInput.value = num.slice(0,3);
-        praceMondeyByn.innerHTML = `${num * 15}<span> РУБ.</span>`;
+        inputSum();
     });
+    function inputSum () {
+        square = praceMondeyInput.value;
+        if(square.length > 3) praceMondeyInput.value = square.slice(0,3);
+        if(square < 18) {
+            praceM2 = 1.9;
+        }else{
+            praceM2 = 3.1;
+        }
+        praceMondeyByn.innerHTML = `${Math.floor(square * praceM2 * cursUsd + pracePerimetr * cursUsd * square * 0.8 + square * works)}<span> РУБ.</span>`;
+    }
 }());
 
 //конец страницы
